8a1974b305574e4d6d1b0d73aaaf1721f37209fc
p-anja <73345582+p-anja@users.noreply.github.com>
2021-12-17 21:28:24 +0100

    feature: user database implemented

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO user (id, name, surname, address, city, phone_number, email, password, type)
+              VALUES (1,'Miroslav','Kojic', 'Dunavska 8', 'Novi Sad', '063563965','miroslav@maildrop.cc',
+                      '$2y$12$NH2KM2BJaBl.ik90Z1YqAOjoPgSd0ns/bF.7WedMxZ54OhWQNNnh6','ADMIN');
+INSERT INTO user (id, name, surname, address, city, phone_number, email, password, type)
+VALUES (2,'Tamara','Simic', 'Narodnog fronta 4', 'Novi Sad', '065856963','tamara@maildrop.cc',
+        '$2y$12$DRhCpltZygkA7EZ2WeWIbewWBjLE0KYiUO.tHDUaJNMpsHxXEw9Ky','CLIENT');
+INSERT INTO user (id, name, surname, address, city, phone_number, email, password, type)
+VALUES (3,'Petar','Babic', 'Milovana Glisica 8', 'Novi Sad', '062356985','petar@maildrop.cc',
+        '$2y$12$i6/mU4w0HhG8RQRXHjNCa.tG2OwGSVXb0GYUnf8MZUdeadE4voHbC','BOAT_OWNER');
